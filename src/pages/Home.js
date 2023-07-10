e918e64b050e459088412bb6301d6dd72dea34c9
GugoGyumri <gugo.grigoryan.97@mail.ru>
2023-07-10 14:13:24 +0400

    Home page join section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,33 @@ import Wrapper from '../layouts/Wrapper';
 
 function Home() {
   return (
-    <Wrapper />
+    <Wrapper>
+      <section className="join">
+        <div className="container">
+          <div className="join__row">
+            <div className="join__left">
+              <h1 className="join__left__title">
+                Join CashTime as a Client or a Freelancer And Start Making Money Easily
+              </h1>
+              <p className="join__left__text">
+                CashApp is The only Armenian Freelance
+                App That Will Allow You To find a Specialist Of
+                any Kind Within Seconds, and Freelancers to Find a Job
+              </p>
+              <div className="join__left__block join-block">
+                <button type="button" className="join-block__btn1">
+                  Sign Up as a Freelancer
+                </button>
+                <button type="button" className="join-block__btn2">
+                  Sign Up as a Client
+                </button>
+              </div>
+            </div>
+            <div className="join__right" />
+          </div>
+        </div>
+      </section>
+    </Wrapper>
   );
 }
 
