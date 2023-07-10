905b6af7f3868d5e6ec3c4ea5c6a16d9845d9220
GevorgKartshikyan <karchikyan.00@mail.ru>
2023-07-10 15:08:30 +0400

    create job form firts section

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './assets/styles/main.scss';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
