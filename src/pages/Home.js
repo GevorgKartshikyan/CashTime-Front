d37c0ba9cfc0de3297d7bf0957185de071ac991b
GugoGyumri <gugo.grigoryan.97@mail.ru>
2023-07-11 12:11:05 +0400

    header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Wrapper from '../layouts/Wrapper';
-import JoinImg from '../assets/images/homejoin.svg';
+import { ReactComponent as JoinImg } from '../assets/images/homejoin.svg';
+import Button from '../components/Button';
+import { ReactComponent as JobLocationIcon } from '../assets/images/home_location_icon.svg';
+import { ReactComponent as SearchIcon } from '../assets/images/Search_Icon.svg';
 
 function Home() {
   return (
@@ -19,20 +22,29 @@ function Home() {
                   any Kind Within Seconds, and Freelancers to Find a Job
                 </p>
                 <div className="join__left__block join-block">
-                  <button type="button" className="join-block__btn1">
-                    Sign Up as a Freelancer
-                  </button>
-                  <button type="button" className="join-block__btn2">
-                    Sign Up as a Client
-                  </button>
+                  <Button className="join-block__btn1" title="Sign Up as a Freelancer" />
+                  <Button className="join-block__btn2" title="Sign Up as a Client" />
                 </div>
               </div>
               <div className="join__right">
-                <img src={JoinImg} alt="IMG" className="join__right__img" />
+                <JoinImg className="join__right__img" />
               </div>
             </div>
             <div className="join__bottom">
               <h3 className="join__bottom__title">Find a Part Time Job Of Your Choice , Easy and Quick</h3>
+              <form className="join__bottom__form">
+                <label htmlFor="input-job" className="join__bottom__form__label">
+                  <input type="text" maxLength="15" className="join__bottom__form__job" id="input-job" placeholder="Job Titile" />
+                </label>
+                <span className="join__bottom__form__line" />
+                <label htmlFor="input-city" className="join__bottom__form__label">
+                  <JobLocationIcon className="join__bottom__form__icon" />
+                  <input type="text" maxLength="15" className="join__bottom__form__city" id="input-city" placeholder="City Name" />
+                </label>
+                <button type="submit" className="join__bottom__form__button">
+                  <SearchIcon className="join__bottom__form__button__icon" />
+                </button>
+              </form>
             </div>
           </div>
         </div>
