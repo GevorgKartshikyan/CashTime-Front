619e72e0b61645a0895ac43ea074f83791f28d6e
Rudo <rud447sin@gmail.com>
2023-07-12 13:54:29 +0400

    verify verified

---
+++ src/pages/Verify-email.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Wrapper from '../layouts/Wrapper';
+import VerifyImg from '../assets/images/verify_img.png';
+
+function VerifyEmail() {
+  const [code, setCode] = useState('');
+  const [isCodeCorrect, setIsCodeCorrect] = useState(true);
+  const navigate = useNavigate();
+
+  const handleChange = (event) => {
+    setCode(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (code === '1234') {
+      setIsCodeCorrect(true);
+      navigate('/verified');
+    } else {
+      setIsCodeCorrect(false);
+    }
+  };
+
+  return (
+    <Wrapper>
+      <div className="verify__email">
+        <div className="container">
+          <div className="verify__email__row">
+            <h2 className="verify__email__text">Verify Your Email</h2>
+            <img src={VerifyImg} className="verify__email__img" alt="Verify__Img" />
+            <p className="verify__email__text__two">
+              Please enter the 4 digit Code Sent to Your Email
+              <br />
+              Youremailexaple.com
+            </p>
+            <form className="verify__email__form" onSubmit={handleSubmit}>
+              <input
+                type="text"
+                value={code}
+                maxLength="4"
+                onChange={handleChange}
+                className={`verify__email__input ${!isCodeCorrect ? 'verify__email__input__error' : ''}`}
+              />
+              {!isCodeCorrect
+                && <p className="verify__email__error">Incorrect code. Please try again.</p>}
+              <p className="verify__email__resend">Resend Code</p>
+              <button type="submit" className="verify__email__submit">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
+}
+
+export default VerifyEmail;
