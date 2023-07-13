a016e73b852cc1b2dd15fac88ba6824aafc039aa
Narek <n-ginosyan@mail.ru>
2023-07-13 12:41:44 +0400

    create history page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import History from './pages/History';
-// import App from './App';
+import App from './App';
 import './assets/styles/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <History />,
+  <App />,
 );
 
 // If you want to start measuring performance in your app, pass a function
