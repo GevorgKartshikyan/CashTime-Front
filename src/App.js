b1eb6d4190427ff60ff7b3201702b5843d6117bd
GugoGyumri <gugo.grigoryan.97@mail.ru>
2023-07-13 15:02:45 +0400

    register form from Gugo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
+// import Home from './pages/Home';
+import SignUp from './pages/Sign-Up';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
