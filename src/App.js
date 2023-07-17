3451826e57a9262de700f31b8da3c93427c09be3
Narek <n-ginosyan@mail.ru>
2023-07-17 11:56:20 +0400

    a

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import VerifyEmail from './pages/Verify-email';
 import Verified from './pages/Verified';
+import ConnectCard from './pages/Connect-card';
+import History from './pages/History';
+import SignUp from './pages/Sign-Up';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/verify" element={<VerifyEmail />} />
         <Route path="/verified" element={<Verified />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/connect-card" element={<ConnectCard />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/History.js
+++ src/pages/History.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import HistoryBlock from '../components/History-block';
+import Header from '../layouts/Header';
 
 function History() {
   return (
     <div className="history">
+      <Header />
       <div className="container">
         <div className="history__row">
           <HistoryBlock />
