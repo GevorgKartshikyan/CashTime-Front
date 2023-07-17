f3558387814e0aa564911b7c7f9c773788cd616d
Rudo <rud447sin@gmail.com>
2023-07-17 12:27:25 +0400

    nothing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import VerifyEmail from './pages/Verify-email';
-import Verified from './pages/Verified';
 import CreateJob from './pages/Create-job';
+import WriteReview from './pages/Write-Review';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/verify" element={<VerifyEmail />} />
-        <Route path="/verified" element={<Verified />} />
+        <Route path="/writereview" element={<WriteReview />} />
         <Route path="/create" element={<CreateJob />} />
       </Routes>
     </BrowserRouter>
