031b5e8cc957db5a21094e70ad7f570723ea44f5
Hovhannes Galstyan <galstyaannn@gmail.com>
2023-07-17 14:16:01 +0400

    messages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import VerifyEmail from './pages/Verify-email';
 import Verified from './pages/Verified';
+import Messages from './pages/Messages';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/verify" element={<VerifyEmail />} />
         <Route path="/verified" element={<Verified />} />
+        <Route path="/messages" element={<Messages />} />
       </Routes>
     </BrowserRouter>
   );
