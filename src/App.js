e93908a30749f0c99400ea84b7302856bbce53bd
vashikmatevosyan <vashik531@gmail.com>
2023-07-31 12:38:55 +0400

    translate , oferr update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import i18n from 'i18next';
 import Home from './pages/Home';
 import CreateJob from './pages/Create-job';
 import WriteReview from './pages/Write-Review';
@@ -9,8 +10,12 @@ import VerifyEmail from './pages/Verify-email';
 import Verified from './pages/Verified';
 import ConnectCard from './pages/Connect-card';
 import History from './pages/History';
+import WorkerOffers from './pages/Worker_Offers';
 
 function App() {
+  useEffect(() => {
+    i18n.changeLanguage(window.localStorage.getItem('language'));
+  }, [window.localStorage.getItem('language')]);
   return (
     <BrowserRouter>
       <Routes>
@@ -25,6 +30,7 @@ function App() {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/connect-card" element={<ConnectCard />} />
         <Route path="/create-job" element={<CreateJob />} />
+        <Route path="/worker-offers" element={<WorkerOffers />} />
       </Routes>
     </BrowserRouter>
   );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
+import { I18nextProvider } from 'react-i18next';
 import reportWebVitals from './reportWebVitals';
 import './assets/styles/index.scss';
 import App from './App';
+import i18n from './18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />,
+  <I18nextProvider i18n={i18n}>
+    <Suspense fallback={<div>Loading...</div>}>
+      <App />
+    </Suspense>
+  </I18nextProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
