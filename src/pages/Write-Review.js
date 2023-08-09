56d1f1e904c7137d71b3f0f9dda3dd4fb294bb0e
Rudo <rud447sin@gmail.com>
2023-08-09 14:19:56 +0400

    accordion

--- src/pages/Write-Review.js
+++ src/pages/Write-Review.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import Wrapper from '../layouts/Wrapper';
-import AddService from '../components/addService';
 
 function WriteReview() {
   // State to keep track of the selected star count
@@ -13,7 +12,6 @@ function WriteReview() {
 
   return (
     <Wrapper>
-      <AddService />
       <div className="write__review">
         <div className="container">
           <div className="write__review__row">
