43f101b26abbb7accc7ffebc528d4e47e872a2d8
Rudo <rud447sin@gmail.com>
2023-08-28 14:30:03 +0400

    css

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,11 +2,8 @@ import createJobForm from './createJobForm';
 import createCvForm from './createCvForm';
 import phoneErrorHandler from './phoneErrorHandler';
 import messages from './messages';
-<<<<<<< HEAD
-import jobsRequest from './jobsRequest';
-=======
 import users from './users';
->>>>>>> b281f4c241286e30f7c0c132ca41a7b5c1f111c1
+import jobsRequest from './jobsRequest';
 
 export default {
   messages,
